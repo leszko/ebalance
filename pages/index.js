22adf845390dee4844024bf0f5129e8464f11030
Rafał Leszko <rafal@livepeer.org>
2022-07-19 16:27:43 +0200

    Fetch data from real endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
 import Head from "next/head";
+const http = require("http");
 
-export default function Home() {
-  const data = [
-    { date: "14.07.2022", ethBalance: "0.12" },
-    { date: "15.07.2022", ethBalance: "0.11" },
-    { date: "16.07.2022", ethBalance: "0.04" },
-    { date: "17.07.2022", ethBalance: "0.06" },
-    { date: "18.07.2022", ethBalance: "0.04" },
-    { date: "19.07.2022", ethBalance: "0.05" },
-  ];
+export async function getServerSideProps(context) {
+  const res = await fetch("http://localhost:3000/api/balances/1234");
+  const data = await res.json();
 
-  let tableContent = "";
-  data.forEach(d => {
-    tableContent+= `<tr><td>${d.date}</td><td>${d.ethBalance}</td><td></td></tr>`;
-  });
+  return { props: { data } };
+}
+
+export default function Main({data}) {
+  console.log(data);
 
   return (
     <div>
@@ -25,7 +21,7 @@ export default function Home() {
         <p>Get started by editing.</p>
 
         <div>
-          <table border="1"> 
+          <table border="1">
             <tbody>
               <tr>
                 <th>Date</th>
@@ -34,35 +30,34 @@ export default function Home() {
               </tr>
               <tr>
                 <td>{data[0].date}</td>
-                <td>{data[0].ethBalance}</td>
+                <td>{data[0].avgGwei}</td>
                 <td></td>
               </tr>
               <tr>
                 <td>{data[1].date}</td>
-                <td>{data[1].ethBalance}</td>
+                <td>{data[1].avgGwei}</td>
                 <td></td>
               </tr>
               <tr>
                 <td>{data[2].date}</td>
-                <td>{data[2].ethBalance}</td>
+                <td>{data[2].avgGwei}</td>
                 <td></td>
               </tr>
-              <tr>
+              {/* <tr>
                 <td>{data[3].date}</td>
-                <td>{data[3].ethBalance}</td>
+                <td>{data[3].avgGwei}</td>
                 <td></td>
               </tr>
               <tr>
                 <td>{data[4].date}</td>
-                <td>{data[4].ethBalance}</td>
+                <td>{data[4].avgGwei}</td>
                 <td></td>
               </tr>
               <tr>
                 <td>{data[5].date}</td>
-                <td>{data[5].ethBalance}</td>
+                <td>{data[5].avgGwei}</td>
                 <td></td>
-              </tr>
-              
+              </tr> */}
             </tbody>
           </table>
         </div>

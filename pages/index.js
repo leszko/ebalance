6234e26d64f3428aed0784763abe1516999cf8b2
Rafał Leszko <rafal@livepeer.org>
2022-07-20 11:20:08 +0200

    Update UI with the proper table rendering

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,29 @@ import Head from "next/head";
 const http = require("http");
 
 function formatGwei(gwei) {
-    const eth = gwei / 1000000000;
-    return eth.toFixed(6);
+  const eth = gwei / 1000000000;
+  return eth.toFixed(6);
 }
 
-const apiUrl =   process.env.API_URL ? process.env.API_URL : "http://localhost:3000";
+const apiUrl = process.env.API_URL
+  ? process.env.API_URL
+  : "http://localhost:3000";
 
 export async function getServerSideProps(context) {
   const address = context.query.address;
   if (!address) {
-    return { props: {}};
+    return { props: {} };
   }
   const balances = await fetch(`${apiUrl}/api/balances/${address}`);
   const data = await balances.json();
-  const result = data.map(b => {return {date: b.date, avgGwei: formatGwei(b.avgGwei)}})
-
+  const result = data.map((b) => {
+    return { date: b.date, avgGwei: formatGwei(b.avgGwei) };
+  });
 
   return { props: { data: result } };
 }
 
-export default function Main({data}) {
+export default function Main({ data }) {
   return (
     <div>
       <Head>
@@ -29,8 +32,6 @@ export default function Main({data}) {
       </Head>
 
       <main>
-        <p>Get started by editing.</p>
-
         <div>
           <table border="1">
             <tbody>
@@ -39,36 +40,13 @@ export default function Main({data}) {
                 <th>ETH</th>
                 <th>Description</th>
               </tr>
-              <tr>
-                <td>{data[0].date}</td>
-                <td>{data[0].avgGwei}</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>{data[1].date}</td>
-                <td>{data[1].avgGwei}</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>{data[2].date}</td>
-                <td>{data[2].avgGwei}</td>
-                <td></td>
-              </tr>
-              {/* <tr>
-                <td>{data[3].date}</td>
-                <td>{data[3].avgGwei}</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>{data[4].date}</td>
-                <td>{data[4].avgGwei}</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>{data[5].date}</td>
-                <td>{data[5].avgGwei}</td>
-                <td></td>
-              </tr> */}
+              {data.map((d, index) => (
+                <tr key="{d.date}">
+                  <td>{d.date}</td>
+                  <td>{d.avgGwei}</td>
+                  <td></td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
